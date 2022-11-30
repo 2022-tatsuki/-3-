943579df71a010e770f7d6ab45e1645ed4aaa921
2022-tatsuki <119149738+2022-tatsuki@users.noreply.github.com>
2022-11-30 22:33:49 +0900

    第3回目課題修正
    マップにCharacterクラスのインスタンスをいれた。
    例外処理を修正した。

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,36 @@
+//mapの使用を宣言
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
 
-        Map<Integer,String> userMember = new HashMap<>();
-        userMember.put(1,"三雲修。中学生。レイガスト使い");
-        userMember.put(2,"空閑遊真。中学生。スコーピオン使い");
-        userMember.put(3,"雨取千佳。小学生。スナイパー");
-        userMember.put(4,"迅悠一。高校生。風刃使い");
+        //mapを設定
+        Map<Integer, Character> Characterlist = new HashMap<>();
+        Characterlist.put(1, new Character("三雲　修", 15, "B級", "レイガスト使い", "ノーマルトリガー"));
+        Characterlist.put(2, new Character("空閑　遊真", 15, "B級", "スコーピオン使い", "ブラックトリガー"));
+        Characterlist.put(3, new Character("雨取　千佳", 13, "B級", "イーグレット使い", "ノーマルトリガー"));
+        Characterlist.put(4, new Character("迅　悠一", 19, "S級", "風刃使い", "ブラックトリガー"));
 
-        System.out.println(userMember.get(1));
-        System.out.println(userMember.get(2));
-        System.out.println(userMember.get(3));
-        System.out.println(userMember.get(4));
-
-        int id =5;
-
-        try {
-        if(id < 0 || 4 < id) {
-            throw new ArrayIndexOutOfBoundsException();
+        //名前のみ表示
+        for (int i = 1; i < 5; i++) {
+            Characterlist.get(i).dispName();
         }
-        System.out.println(userMember.get(5));
-        } catch (ArrayIndexOutOfBoundsException  e ){
-           System.out.println("そのメンバーは存在しません");
+        //mapを全て表示させる。
+        for (int i = 1; i < 5; i++) {
+            Characterlist.get(i).dispCharacterDirectory();
+        }
+
+
+        //例外を意図的に表示　
+        try {
+            if (Characterlist.get(0) == null) {
+                throw new OriginalException("そのメンバーは存在しません");
+            }
+
+        } catch (OriginalException e) {
+            System.out.println(e.getError());
         }
     }
 }
